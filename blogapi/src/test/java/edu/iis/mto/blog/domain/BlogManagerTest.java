7b69e8e6870dc624dca6412081a26e094e4820d6
Krzysztof Chybicki <krysiek97@interia.eu>
2020-06-05 00:31:32 +0200

    added behaviour test for addLikeToPost method and added error handling in that method

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,9 +1,17 @@
 package edu.iis.mto.blog.domain;
 
 import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -20,12 +28,20 @@ import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.BlogDataMapper;
 import edu.iis.mto.blog.services.BlogService;
 
+import java.util.Optional;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
 
     @MockBean
     private UserRepository userRepository;
+
+    @MockBean
+    private BlogPostRepository blogPostRepository;
+
+    @MockBean
+    private LikePostRepository likePostRepository;
 
     @Autowired
     private BlogDataMapper dataMapper;
@@ -36,12 +52,54 @@ public class BlogManagerTest {
     @Captor
     private ArgumentCaptor<User> userParam;
 
+    @Captor
+    private ArgumentCaptor<BlogPost> blogParam;
+
+    private BlogPost blogPost;
+    private User user, otherUser;
+
+    @Before
+    public void setUp() throws Exception {
+        Long userID = 5L;
+        Long otherUserID = 9L;
+        Long blogPostID = 3L;
+
+        user = new User();
+        user.setId(userID);
+
+        otherUser = new User();
+        otherUser.setId(otherUserID);
+
+        blogPost = new BlogPost();
+        blogPost.setId(blogPostID);
+        blogPost.setUser(otherUser);
+
+        when(userRepository.findById(userID)).thenReturn(Optional.of(user));
+        when(userRepository.findById(otherUserID)).thenReturn(Optional.of(otherUser));
+        when(blogPostRepository.findById(blogPostID)).thenReturn(Optional.of(blogPost));
+    }
+
     @Test
     public void creatingNewUserShouldSetAccountStatusToNEW() {
         blogService.createUser(new UserRequest("John", "Steward", "john@domain.com"));
         verify(userRepository).save(userParam.capture());
         User user = userParam.getValue();
         assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
+    }
+
+    @Test
+    public void addingLikePostShouldNotThrowForUsersWithStatusCONFIRMED() {
+        user.setAccountStatus(AccountStatus.CONFIRMED);
+        assertDoesNotThrow(() -> blogService.addLikeToPost(user.getId(), blogPost.getId()));
+    }
+
+    @Test
+    public void addingLikePostShouldThrowForUsersWithStatusNotCONFIRMED() {
+        user.setAccountStatus(AccountStatus.NEW);
+        assertThrows(DomainError.class, () -> blogService.addLikeToPost(user.getId(), blogPost.getId()));
+
+        user.setAccountStatus(AccountStatus.REMOVED);
+        assertThrows(DomainError.class, () -> blogService.addLikeToPost(user.getId(), blogPost.getId()));
     }
 
 }
